86867600fe07f7f0c8c3451824655b0432e147c6
Alex <chukrii.alex@gmail.com>
2022-05-17 00:32:06 +0200

    Server: login, registration implemented

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const config = require('config')
 const PORT = config.get('serverPort')
 const app = express()
 
+const authRouter = require('./routes/auth.routes')
+
+app.use(express.json())
+app.use('/api/auth', authRouter)
+
 const start = async () => {
     try {
         mongoose.connect(config.get('mongoAtlas'))
